dfc52e89757d8ec7fb1dc03ec860160571a0ec6d
John Otieno <jomakonyango@gmail.com>
2024-01-23 14:06:23 -0800

    Compare 2 squares

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,29 +1,59 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square that defines a square by size and provides
+methods to calculate its area. The Square instance can answer to comparators
+based on the square area.
+"""
+
 class Square:
-    """Defines a square by size and provides methods to calculate its area."""
+    """
+    A class to represent a square.
+
+    ...
+
+    Attributes
+    ----------
+    size : int or float
+        size of the square
+
+    Methods
+    -------
+    area():
+        Returns the area of the square.
+    """
 
     def __init__(self, size=0):
-        """Initializes a new Square instance with a given size.
+        """
+        Constructs all the necessary attributes for the square object.
 
-        Args:
-            size (float or int): The size of the new Square instance.
+        Parameters
+        ----------
+            size : int or float, optional
+                size of the square (default is 0)
         """
         self.size = size
 
     @property
     def size(self):
-        """Gets the size of the Square instance."""
+        """Gets the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size of the Square instance.
+        """
+        Sets the size of the square.
 
-        Args:
-            value (float or int): The size to set for the Square instance.
+        Parameters
+        ----------
+            value : int or float
+                size of the square
 
-        Raises:
-            TypeError: If `value` is not a number.
-            ValueError: If `value` is less than 0.
+        Raises
+        ------
+        TypeError
+            If size is not a number.
+        ValueError
+            If size is less than 0.
         """
         if not isinstance(value, (int, float)):
             raise TypeError("size must be a number")
