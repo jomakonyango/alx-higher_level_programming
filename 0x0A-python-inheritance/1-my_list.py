5de6c9bb4d26486ba83320c4c9b2bdaa481ecd36
John Otieno <jomakonyango@gmail.com>
2024-02-06 00:29:39 +0300

    my list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains a class called 'MyList' that inherits from the built-in list class.
+"""
+
+class MyList(list):
+    """
+    A class that inherits from list and has a method to print the list sorted in ascending order.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list sorted in ascending order.
+        """
+        print(sorted(self))
+
