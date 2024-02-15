20edecb6ee25540664efe2403e83dddc7eaaac2d
John Otieno <jomakonyango@gmail.com>
2024-02-15 06:41:04 +0300

    rectangle class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+This module contains the definition of a simple class, Rectangle.
+
+The Rectangle class is currently empty and serves as a placeholder for
+future attributes and methods.
+"""
+
 class Rectangle:
+    """
+    An empty class that defines a rectangle.
+
+    This class does not do anything and does not contain any attributes or methods.
+    """
     pass
