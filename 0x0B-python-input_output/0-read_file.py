0f024c0aac12da8a02133449608e297de41401ec
John Otieno <jomakonyango@gmail.com>
2024-03-05 10:40:43 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-This module contains a function that reads a text file (UTF8) and prints it to stdout.
+This module contains a function that reads a text file (UTF8) and prints it to
+stdout.
 """
+
 
 def read_file(filename=""):
     """
