a9b1c564ee119766bf008c4e023fa1b5e8bcd255
John Otieno <jomakonyango@gmail.com>
2024-03-05 23:41:44 +0300

    class json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module: 8-class_to_json
+
+This module contains a function that returns the dictionary description with
+simple data structure (list, dictionary, string, integer and boolean) for JSON
+serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """
+    Function: class_to_json
+
+    This function returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization of
+    an object.
+
+    Parameters:
+    - obj: An instance of a Class. All attributes of the obj Class are
+           serializable: list, dictionary, string, integer and boolean.
+
+    Returns:
+    - The dictionary description of obj.
+    """
+    return obj.__dict_
