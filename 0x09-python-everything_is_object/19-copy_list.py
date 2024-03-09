a81bcf62be94a363e365223f3181d154ab9aa94a
John Otieno <jomakonyango@gmail.com>
2024-03-09 10:36:11 +0300

    copy list using slicing

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+def copy_list(l):
+    """
+    This function returns a copy of a list.
+
+    Args:
+        l (list): The list to be copied.
+
+    Returns:
+        list: A copy of the input list.
+    """
+    return l[:]
