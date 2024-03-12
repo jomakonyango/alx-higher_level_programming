3da07f7fb88d85ccf7d3cd750bbd24b4abc5cfe2
John Otieno <jomakonyango@gmail.com>
2024-03-12 03:18:16 +0300

    retactangle Dispaly  method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,8 @@ class Rectangle(Base):
     def area(self):
         """Method that returns the area of the Rectangle instance."""
         return self.__width * self.__height
+
+    def display(self):
+        """Method that prints in stdout the Rectangle instance with the character #."""
+        for i in range(self.__height):
+            print("#" * self.__width)
