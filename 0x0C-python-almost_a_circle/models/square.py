b8acfd2e93a9e7573f11c0545b19be5db6665d9e
John Otieno <jomakonyango@gmail.com>
2024-03-13 11:31:57 +0300

    public setter getter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,34 @@ class Square(Rectangle):
         """Constructor."""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
-        """Method that returns the string representation of the Square instance."""
+        """Method that returns the string
+        representation of the Square instance."""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """Method that assigns an argument to each attribute."""
+        attributes = ["id", "width", "height", "x", "y"]
+        if args and len(args) > 0:
+            for i, arg in enumerate(args):
+                setattr(self, attributes[i], arg)
+        else:
+            for key, value in kwargs.items():
+                if key in attributes:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Method that returns the dictionary
+        representation of a Square."""
+        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
