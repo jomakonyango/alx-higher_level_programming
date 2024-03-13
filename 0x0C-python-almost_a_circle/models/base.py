5d42551a8e5205b42c29f0d3bd305056317a1d4f
John Otieno <jomakonyango@gmail.com>
2024-03-13 12:50:30 +0300

    instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,3 +37,10 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set."""
+        dummy = cls(1, 1)
+        dummy.update(**dictionary)
+        return dummy
