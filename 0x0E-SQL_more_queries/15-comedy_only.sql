3bef7d5e933b2dc39ff63bbf265980c5f8a4aec3
John Otieno <jomakonyango@gmail.com>
2024-03-15 01:06:42 +0300

    list comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,7 @@
+-- This script lists all Comedy shows in the database hbtn_0d_tvshows.
+-- Each record displays: tv_shows.title
+-- Results are sorted in ascending order by the show title.
+
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
