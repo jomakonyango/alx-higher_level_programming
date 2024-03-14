7de94bb93de2d43439d21a2327939ea195786b2d
John Otieno <jomakonyango@gmail.com>
2024-03-15 01:26:19 +0300

    lists all genres in the database

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,9 @@
+-- This script lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Each record displays: tv_shows.title - rating sum
+-- Results are sorted in descending order by the rating.
+
+SELECT tv_shows.title, SUM(rating) AS rating
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
