f4bf7a0c845d2105dfa6453ff927ce6b5f772ea3
John Otieno <jomakonyango@gmail.com>
2024-03-17 11:38:53 +0300

    create and convert

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,9 @@
+USE hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS first_table (
+	    id INT,
+	    name VARCHAR(256)
+	);
+
 -- Converts the hbtn_0c_0 database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
