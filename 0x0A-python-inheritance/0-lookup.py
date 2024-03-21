2d9ccf90865a6f465b8518617d68194bc87cb741
John Otieno <jomakonyango@gmail.com>
2024-03-21 04:58:13 +0300

    returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """
-This module contains a function called 'lookup' that returns a list of available
+This module contains a function
+called 'lookup' that returns a list of available
 attributes and methods of an object.
 """
 
+
 def lookup(obj):
     """
-    Function that returns the list of available attributes and methods of an object.
+    Function that returns the list of
+    available attributes and methods of an object.
 
     Parameters:
     obj: The object whose attributes and methods we want to list.
@@ -15,4 +18,3 @@ def lookup(obj):
     A list of the names of the attributes and methods of the object.
     """
     return dir(obj)
-
