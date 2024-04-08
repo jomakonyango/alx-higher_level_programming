332ed55ff9b1a3d7f2ad7f6b27114be1f092483b
John Otieno <jomakonyango@gmail.com>
2024-04-09 01:56:16 +0300

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (!n || n < 2) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
