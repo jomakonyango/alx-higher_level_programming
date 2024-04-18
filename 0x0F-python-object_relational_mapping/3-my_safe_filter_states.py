362f5ea84d24e14302edabd85f423549adee6174
John Otieno <jomakonyango@gmail.com>
2024-04-18 23:49:46 +0300

    Arizona

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+3-my_safe_filter_states.py
+
+Script that lists all states from the database hbtn_0e_0_usa where name matches
+the argument, safe from MySQL injections.
+"""
+
+import MySQLdb
+import sys
+
+
+def list_states():
+    """
+    Lists all states from the database hbtn_0e_0_usa where name matches the
+    argument, safe from MySQL injections.
+    """
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    list_states()
