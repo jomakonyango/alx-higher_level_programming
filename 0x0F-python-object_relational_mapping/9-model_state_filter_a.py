e704043af9b8023ffe60bb85b6f0c710722d165a
John Otieno <jomakonyango@gmail.com>
2024-04-19 01:11:03 +0300

    9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+9-model_state_filter_a.py
+
+Script that lists all State objects that contain the letter 'a' from the
+database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def list_states_with_a():
+    """
+    Lists all State objects that contain the letter 'a' from the
+    database hbtn_0e_6_usa.
+    """
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).filter(
+            State.name.contains('a')).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
+
+
+if __name__ == "__main__":
+    list_states_with_a()
