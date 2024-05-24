a4fbfce785768ded07b6056f5f49298f6f99da3b
John Otieno <jomakonyango@gmail.com>
2024-05-24 06:31:23 +0300

    star wars characters

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    const characterUrls = movie.characters;
+
+    printCharacters(characterUrls, 0);
+  }
+});
+
+function printCharacters(characterUrls, i) {
+  if (i >= characterUrls.length) {
+    return;
+  }
+
+  request(characterUrls[i], function (error, response, body) {
+    if (error) {
+      console.error(error);
+    } else {
+      const character = JSON.parse(body);
+      console.log(character.name);
+
+      printCharacters(characterUrls, i + 1);
+    }
+  });
+}
